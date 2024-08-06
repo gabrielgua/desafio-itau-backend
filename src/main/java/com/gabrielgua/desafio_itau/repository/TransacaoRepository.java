8121db41042ea2ff9aee7fc333c82812b5f45fe1
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-08-06 15:28:31 -0300

    chore: add loggin info save, clear and calculate operations

--- src/main/java/com/gabrielgua/desafio_itau/repository/TransacaoRepository.java
+++ src/main/java/com/gabrielgua/desafio_itau/repository/TransacaoRepository.java
@@ -1,26 +1,28 @@
 package com.gabrielgua.desafio_itau.repository;
 
 import com.gabrielgua.desafio_itau.model.Transacao;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
+@Slf4j
 public class TransacaoRepository {
 
     private final List<Transacao> transacoes = new ArrayList<>();
 
+    public List<Transacao> listAll() { return transacoes; }
 
     public void save(Transacao transacao) {
-        System.out.println("Added - " + transacao.getValor() + ", " + transacao.getDataHora());
         transacoes.add(transacao);
+        log.info("Transação adicionada");
     }
 
     public void clear() {
         transacoes.clear();
+        log.warn("Transações limpadas");
     }
-    public List<Transacao> listAll() {
-        return transacoes;
-    }
+
 }
--- src/main/java/com/gabrielgua/desafio_itau/service/EstatisticaService.java
+++ src/main/java/com/gabrielgua/desafio_itau/service/EstatisticaService.java
@@ -3,6 +3,7 @@ package com.gabrielgua.desafio_itau.service;
 import com.gabrielgua.desafio_itau.model.Transacao;
 import com.gabrielgua.desafio_itau.repository.TransacaoRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -11,12 +12,14 @@ import java.util.DoubleSummaryStatistics;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EstatisticaService {
 
     private final TransacaoRepository repository;
     private static final Integer INTERVAL = 60;
 
     public DoubleSummaryStatistics calculate() {
+        log.info("Calculando estatísticas");
         var transacoes = repository.listAll();
 
         return transacoes.stream()
